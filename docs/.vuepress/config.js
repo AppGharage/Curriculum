fdb9d0a20d75f0fa68ba1242d4c2c7cd1afafb36
Robbie <akotoro22@gmail.com>
2018-08-25 21:24:10 +0000

    Created projects folder

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,62 +1,59 @@
 module.exports = {
-  title: 'AppGharage',
-  description: 'The Curriculum for Web Developers',
-  serviceWorker: true,
-  themeConfig: {
-    repo: 'AppGharage/Curriculum',
-    repoLabel: 'Contribute!',
-    docsDir: 'docs',
-    editLinks: true,
-    editLinkText: 'Edit this page on Github!',
-    lastUpdated: 'Last Updated',
-    serviceWorker: {
-      updatePopup: true
-    },
-    sidebar:  [
-      {
-        title: 'Main',
-        collapsable: false,
-        children: [
-          '/',
-          '/core/',
-          'frontend/',
-          '/backend/'
+    title: 'AppGharage',
+    description: 'The Curriculum for Web Developers',
+    serviceWorker: true,
+    themeConfig: {
+        repo: 'AppGharage/Curriculum',
+        repoLabel: 'Contribute!',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Edit this page on Github!',
+        lastUpdated: 'Last Updated',
+        serviceWorker: {
+            updatePopup: true
+        },
+        sidebar: [{
+            title: 'Main',
+            collapsable: false,
+            children: [
+                '/',
+                '/core/',
+                'frontend/',
+                '/backend/'
+            ]
+        }],
+        displayAllHeaders: true,
+        nav: [
+            { text: 'Home', link: '/' },
+            {
+                text: 'Projects',
+                items: [{
+                        text: 'General',
+                        items: [
+                            { text: 'Beginner', link: '/frontend/' },
+                            { text: 'Intermediate', link: '/frontend/beginner' },
+                            { text: 'Advanced', link: '/frontend/beginner' }
+                        ]
+                    },
+                    {
+                        text: 'Frontend',
+                        items: [
+                            { text: 'Beginner', link: '/frontend/beginner' },
+                            { text: 'Intermediate', link: '/frontend/beginner' },
+                            { text: 'Advanced', link: '/frontend/beginner' }
+                        ]
+                    },
+                    {
+                        text: 'Backend',
+                        items: [
+                            { text: 'Beginner', link: '/backend/beginner' },
+                            { text: 'Intermediate', link: '/backend/beginner' },
+                            { text: 'Advanced', link: '/backend/beginner' }
+                        ]
+                    },
+                ]
+            },
+            { text: 'Visit AppGharage', link: 'https://appgharage.com' }
         ]
-      }
-    ],
-    displayAllHeaders: true,
-    nav: [
-      { text: 'Home', link: '/' },
-      {
-        text: 'Projects',
-        items: [
-          { 
-            text: 'General', 
-            items: [
-              { text: 'Beginner', link: '/frontend/beginner' },
-              { text: 'Intermediate', link: '/frontend/beginner' },
-              { text: 'Advanced', link: '/frontend/beginner' }
-            ] 
-          },
-          { 
-            text: 'Frontend', 
-            items: [
-              { text: 'Beginner', link: '/frontend/beginner' },
-              { text: 'Intermediate', link: '/frontend/beginner' },
-              { text: 'Advanced', link: '/frontend/beginner' }
-            ] 
-          },
-          { 
-            text: 'Backend', 
-            items: [
-              { text: 'Beginner', link: '/backend/beginner' },
-              { text: 'Intermediate', link: '/backend/beginner' },
-              { text: 'Advanced', link: '/backend/beginner' }
-            ] 
-          },
-        ]
-      },
-      { text: 'Visit AppGharage', link: 'https://appgharage.com' }
-    ]
-  }
+    }
 }
---
+++ docs/projects/Readme.md
@@ -0,0 +1,7 @@
+# Projects
+
+##Introduction
+
+>Here is a list of practical projects that anyone can try out in any programming language (See solutions). These projects are divided in >multiple categories, and each category has its own folder.
+
+
